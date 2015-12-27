446c84bb38259809f57643b3126dda8a2ea0125b
SchroSct <clls760@gmail.com>
2015-12-27 07:38:42 -0800

    Create twittercreep.sh

---
+++ twittercreep.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+savepath=""
+creep(){
+username="$1"
+if cd "$savepath"
+then
+   if [[ -d "${username}" ]]
+   then
+      echo "${username} exists, crawling."
+   else
+      mkdir "${username}"
+   fi
+   if cd "${username}"
+   then
+      mapfile -t links < <(curl -s "https://twitter.com/i/profiles/show/${username}/media_timeline?include_available_features=1&include_entities=1&reset_error_state=false" | sed -e 's/\\n/\n/g' -e 's/\\//g' | grep -e "data-src" -e "img src" | sed -e 's/.*data-src="/https:\/\/twitter.com/g' -e 's/\?.*//g' -e 's/.*img src="//g' -e 's/".*//g')
+      for i in "${links[@]}"
+      do
+            if [[ "$i" =~ ".jpg" ]]
+            then
+               wget -nc "$i"
+            else
+               youtube-dl -i "$i"
+            fi
+      done
+   fi
+else
+   echo "TWITTERCREEP FAILED"
+fi
+}
+while true
+do
+   if cd "$savepath"
+   then
+      mapfile -t usernames < <(cat twittercreep.txt)
+      for i in "${usernames[@]}"
+      do
+         creep "$i"
+      done
+   else
+      echo "COULD NOT ENTER DIRECTORY"
+   fi
+   let random=$RANDOM%360
+   let sleepy=45+$random
+   echo "sleeping $sleepy on $(date)"
+   sleep $sleepy
+done
